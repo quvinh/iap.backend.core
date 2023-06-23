5aca5b5de7a34bcb6fefbebbe74ee76c87684e48
vinhnq <vinhnq@absoltech.vn>
2023-06-23 21:48:33 +0700

    invoice resources

---
+++ source/app/DataResources/Invoice/InvoiceResource.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\DataResources\Invoice;
+
+use App\DataResources\BaseDataResource;
+use App\Models\Invoice;
+
+class InvoiceResource extends BaseDataResource
+{
+    /**
+     * @var array|string[]
+     */
+    protected array $fields = [
+        'id',
+        'company_id',
+        'invoice_task_id',
+        'type',
+        'date',
+        'invoie_symbol',
+        'invoice_number_form',
+        'invoice_number',
+        'property',
+        'note',
+        'partner_name',
+        'partner_tax_code',
+        'partner_address',
+        'currency',
+        'currency_price',
+        'sum_money_no_vat',
+        'sum_money_vat',
+        'sum_money_discount',
+        'sum_money',
+        'rounding',
+        'payment_method',
+        'verification_code',
+        'json',
+        'status',
+        'created_by',
+        'created_at',
+    ];
+
+    /**
+     * Return the model class of this resource
+     */
+    public function modelClass(): string
+    {
+        return Invoice::class;
+    }
+
+    /**
+     * Load data for output
+     * @param Invoice $obj
+     * @return void
+     */
+    public function load(mixed $obj): void
+    {
+        parent::copy($obj, $this->fields);
+    }
+}
---
+++ source/app/DataResources/InvoiceDetail/InvoiceDetailResource.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\DataResources\InvoiceDetail;
+
+use App\DataResources\BaseDataResource;
+use App\Models\InvoiceDetail;
+
+class InvoiceDetailResource extends BaseDataResource
+{
+    /**
+     * @var array|string[]
+     */
+    protected array $fields = [
+        'id',
+        'invoice_id',
+        'formula_id',
+        'formula_commodity_id',
+        'formula_material_id',
+        'item_code_id',
+        'product',
+        'unit',
+        'quantity',
+        'price',
+        'vat',
+        'vat_money',
+        'total_money',
+        'warehouse',
+        'main_entity',
+        'visible',
+        'note',
+        'created_by',
+        'created_at',
+    ];
+
+    /**
+     * Return the model class of this resource
+     */
+    public function modelClass(): string
+    {
+        return InvoiceDetail::class;
+    }
+
+    /**
+     * Load data for output
+     * @param InvoiceDetail $obj
+     * @return void
+     */
+    public function load(mixed $obj): void
+    {
+        parent::copy($obj, $this->fields);
+    }
+}
