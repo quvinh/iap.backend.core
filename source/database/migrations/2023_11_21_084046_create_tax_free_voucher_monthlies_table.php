c49fc72c151f351596d877198892f0e91b4c65fe
vinhnq <vinhnq@absoft.com.vn>
2023-11-21 08:52:45 +0700

    tax free voucher monthly (not finished yet)

---
+++ source/database/migrations/2023_11_21_084046_create_tax_free_voucher_monthlies_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tax_free_voucher_monthlies', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('company_detail_tax_free_voucher_id');
+            $table->string('number_account', 10);
+            $table->decimal('original_price', 12, 2)->default(0);
+            $table->integer('amount_month')->default(0);
+            $table->json('meta')->nullable(); # Monthly 1 -> 12
+            $table->json('year_end_balance')->nullable();
+
+            $table->foreign('company_detail_tax_free_voucher_id')->references('id')->on('company_detail_tax_free_vouchers');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tax_free_voucher_monthlies');
+    }
+};
