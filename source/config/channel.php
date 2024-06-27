e446f6be70667e558fffaa6671e6d3f31cd9de53
quvinh <vinhhp2620@gmail.com>
2024-06-27 20:55:00 +0700

    refactor channel.php: $signature

--- source/config/channel.php
+++ source/config/channel.php
@@ -1,25 +1,27 @@
 <?php
 
-use PHPUnit\Exception;
-use Spatie\SslCertificate\SslCertificate;
+// use PHPUnit\Exception;
+// use Spatie\SslCertificate\SslCertificate;
 
 
-$https_url = env('APP_CHANNEL_HTTPS_URL', 'https://www.w3.org');
-try {
-    $cert = SslCertificate::createForHostName($https_url);
-    $signature = json_encode([
-        "issuer" => $cert->getIssuer(),
-        "is_valid" => $cert->isValid(),
-        "valid_from_date" => $cert->validFromDate(),
-        "expiration_date" => $cert->expirationDate(),
-        "fingerprint" => $cert->getFingerprint(),
-        "fingerprint256" => $cert->getFingerprintSha256(),
-        "organization" => $cert->getOrganization(),
-        "serial_number" => ($cert->getSerialNumber())
-    ]);
-}catch (Exception $ex){
-    $signature = "{}";
-}
+// $https_url = env('APP_CHANNEL_HTTPS_URL', 'https://www.w3.org');
+// try {
+//     $cert = SslCertificate::createForHostName($https_url);
+//     $signature = json_encode([
+//         "issuer" => $cert->getIssuer(),
+//         "is_valid" => $cert->isValid(),
+//         "valid_from_date" => $cert->validFromDate(),
+//         "expiration_date" => $cert->expirationDate(),
+//         "fingerprint" => $cert->getFingerprint(),
+//         "fingerprint256" => $cert->getFingerprintSha256(),
+//         "organization" => $cert->getOrganization(),
+//         "serial_number" => ($cert->getSerialNumber())
+//     ]);
+// }catch (Exception $ex){
+//     $signature = "{}";
+// }
+
+$signature = "IAP2024";
 
 $channelHooks = [];
 $hooks = explode(',', env('APP_CHANNEL_HOOKS', ''));
@@ -33,7 +35,7 @@ foreach ($hooks as $hook){
 
 
 return [
-    'https_url' =>  $https_url,
+    // 'https_url' =>  $https_url,
     'signature' => md5($signature),
     'key' => env('APP_CHANNEL_KEY', 'demo'),
     'hooks' => $channelHooks
